da158409c3de40482cef0d33f612432c88680f58
tom8531400 <134759040+tom8531400@users.noreply.github.com>
2024-08-27 11:40:59 +0800

    創建spring-ai

---
+++ src/main/java/com/example/springai01chat/SpringAi01ChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.springai01chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi01ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAi01ChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springai01chat/SpringAi01ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springai01chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAi01ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
